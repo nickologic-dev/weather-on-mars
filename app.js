ec26518f8e7a496a48abbbc78e559c320814d395
nbeilis <nicole.beilis@gmail.com>
2020-12-04 19:48:54 -0500

    set info

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 fetch('https://api.nasa.gov/insight_weather/?api_key=rSeiuzYdFB4Uhvgqt2HuCaIL4dIxezO4ML16lKIY&feedtype=json&ver=1.0')
+
   .then((response) => {
     return response.json()
   })
@@ -11,7 +12,15 @@ fetch('https://api.nasa.gov/insight_weather/?api_key=rSeiuzYdFB4Uhvgqt2HuCaIL4dI
         var temp = data.sol_keys[i]
         console.log(data[temp].First_UTC) // start time on Earth
     }
-    document.getElementById("sol").innerHTML = "Sol # " + data.sol_keys[data.sol_keys.length-1] //set to current sol
+    
+    var current = data.sol_keys[data.sol_keys.length-1]
+    document.getElementById("sol_num").innerHTML = "Sol # " + current //set to current sol
+    document.getElementById("season").innerHTML = "Season: " + data[current].Season //set to current sol
+    document.getElementById("day_start").innerHTML = "Day Start: " + data[current].First_UTC //set to current sol
+    document.getElementById("day_end").innerHTML = "Day End: " + data[current].Last_UTC //set to current sol
+    document.getElementById("pre_avg").innerHTML = "Avg: " + data[current].PRE.av + " Pa" //set to current sol
+    document.getElementById("pre_high").innerHTML = "High: " + data[current].PRE.mx + " Pa" //set to current sol
+    document.getElementById("pre_low").innerHTML = "Low: " + data[current].PRE.mn + " Pa" //set to current sol
 
   })
   .catch((err) => {
