64cce4a63151755cf437b6b327c5c575157dc5bb
nbeilis <nicole.beilis@gmail.com>
2020-12-05 01:04:39 -0500

    reorganize content

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ fetch('https://api.nasa.gov/insight_weather/?api_key=rSeiuzYdFB4Uhvgqt2HuCaIL4dI
     return response.json()
   })
   .then((data) => {
-    // Work with JSON data here
     console.log(data);
+
+    var current = data.sol_keys[data.sol_keys.length - 1];
+
+    var selected = current
 
     for (var i = 0; i < data.sol_keys.length; i++) {
       var solBtn = document.createElement("button");
       solBtn.classList.add("tabs");
+      solBtn.id = i;
+      solBtn.onclick = function(event) {
+        selected = data.sol_keys[solBtn.id];
+        assignData(selected, data);
+      }
 
       var solName = document.createElement("h2");
       solName.innerHTML = "Sol " + data.sol_keys[i];
@@ -25,18 +33,21 @@ fetch('https://api.nasa.gov/insight_weather/?api_key=rSeiuzYdFB4Uhvgqt2HuCaIL4dI
 
       document.getElementById("sol_tabs").appendChild(solBtn);
     }
-    
-    var current = data.sol_keys[data.sol_keys.length - 1];
-    //temporarily set to current sol
-    document.getElementById("sol_title").innerHTML = "Sol # " + current
-    document.getElementById("season").innerHTML = "Season: " + data[current].Season 
-    document.getElementById("day_start").innerHTML = "Day Start: " + data[current].First_UTC
-    document.getElementById("day_end").innerHTML = "Day End: " + data[current].Last_UTC 
-    document.getElementById("pre_avg").innerHTML = "Avg: " + data[current].PRE.av + " Pa" 
-    document.getElementById("pre_high").innerHTML = "High: " + data[current].PRE.mx + " Pa" 
-    document.getElementById("pre_low").innerHTML = "Low: " + data[current].PRE.mn + " Pa" 
+
+    assignData(selected, data)
 
   })
   .catch((err) => {
     // Do something for an error here
   })
+
+  function assignData(selected, data) {
+    console.log(selected)
+    document.getElementById("sol_title").innerHTML = "Sol # " + selected
+    document.getElementById("season").innerHTML = "Season: " + data[selected].Season 
+    document.getElementById("day_start").innerHTML = "Day Start: " + data[selected].First_UTC
+    document.getElementById("day_end").innerHTML = "Day End: " + data[selected].Last_UTC 
+    document.getElementById("pre_avg").innerHTML = "Avg: " + data[selected].PRE.av + " Pa" 
+    document.getElementById("pre_high").innerHTML = "High: " + data[selected].PRE.mx + " Pa" 
+    document.getElementById("pre_low").innerHTML = "Low: " + data[selected].PRE.mn + " Pa" 
+  }
