4e99947f6c58e37f7759d609772cf122fdbc6fea
nbeilis <nicole.beilis@gmail.com>
2020-12-05 00:36:50 -0500

    create tabs

--- app.js
+++ app.js
@@ -5,16 +5,29 @@ fetch('https://api.nasa.gov/insight_weather/?api_key=rSeiuzYdFB4Uhvgqt2HuCaIL4dI
   })
   .then((data) => {
     // Work with JSON data here
-    console.log(data)
+    console.log(data);
+
+    var current = data.sol_keys[data.sol_keys.length - 1];
 
     for (var i = 0; i < data.sol_keys.length; i++) {
-        console.log(data.sol_keys[i]) // date
-        var temp = data.sol_keys[i]
-        console.log(data[temp].First_UTC) // start time on Earth
+      var solBtn = document.createElement("button");
+
+      var solName = document.createElement("h2");
+      solName.innerHTML = "Sol " + data.sol_keys[i];
+      solBtn.append(solName);
+
+      var solStartDate = document.createElement("h3");
+      solStartDate.innerHTML = "Day Start: " + data[data.sol_keys[i]].First_UTC;
+      solBtn.appendChild(solStartDate);
+
+      var solEndDate = document.createElement("h3");
+      solEndDate.innerHTML = "Day End: " + data[data.sol_keys[i]].Last_UTC;
+      solBtn.appendChild(solEndDate);
+
+      document.getElementById("sol_tabs").appendChild(solBtn);
     }
     
-    var current = data.sol_keys[data.sol_keys.length-1]
-    document.getElementById("sol_num").innerHTML = "Sol # " + current //set to current sol
+    document.getElementById("sol_title").innerHTML = "Sol # " + current //set to current sol
     document.getElementById("season").innerHTML = "Season: " + data[current].Season //set to current sol
     document.getElementById("day_start").innerHTML = "Day Start: " + data[current].First_UTC //set to current sol
     document.getElementById("day_end").innerHTML = "Day End: " + data[current].Last_UTC //set to current sol
