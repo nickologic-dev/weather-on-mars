2562731d779264eaa0c21035a4e4e83e1e00b5f6
Vivian Zhu <vivianzhu1211@gmail.com>
2021-01-01 16:10:50 -0500

    wind chart update

--- app.js
+++ app.js
@@ -37,19 +37,35 @@ fetch('https://api.nasa.gov/insight_weather/?api_key=rSeiuzYdFB4Uhvgqt2HuCaIL4dI
       document.getElementById("sol_tabs").appendChild(solBtn);
     }
 
-    assignData(selected, data)
-
     //create wind diagram
+    var dir_array = ["N", "E", "S", "W"];
+    for (var i = 0; i < 4; i++) {
+      var newDir = document.createElement("h2");
+      newDir.className = "direction_name";
+      newDir.setAttribute("style", "--x: " + ((i%2) * (2-i)) + "; --y: " + (((i+1)%2) * (i-1)) + ";");
+      newDir.innerHTML = dir_array[i];
+
+      document.getElementById("wind").appendChild(newDir);
+    }
     for (var i = 0; i < 16; i++) {
       var newDiv = document.createElement("div");
-      var classAtt = document.createAttribute("class");
-      var styleAtt = document.createAttribute("style");
-      classAtt.value = "pie_segment";
-      styleAtt.value = "--offset: " + (3.125 + (i * 6.25)) + "; --value: " + 6.25 + ";"
-      newDiv.setAttributeNode(classAtt);
-      newDiv.setAttributeNode(styleAtt);
-      document.getElementById("wind").appendChild(newDiv);
+      newDiv.className = "pie_segment";
+      newDiv.setAttribute("style", "--offset: " + (i * 6.25) + "; --value: " + 6.25 + ";");
+      document.getElementById("wind_pie").appendChild(newDiv);
+
+      var newCTPie = document.createElement("div");
+      newCTPie.className = "ct_pie";
+      newCTPie.id = "direction_" + [i+1].toString();
+      document.getElementById("wind").appendChild(newCTPie);
+
+      var newSegment = document.createElement("div");
+      newSegment.className = "pie_segment";
+      newSegment.setAttribute("style", "--offset: " + (3.125 + (i * 6.25)) + "; --value: " + 6.25 +
+      "; --bg: aqua");
+      newCTPie.appendChild(newSegment);
     }
+
+    assignData(selected, data)
 
   })
   .catch((err) => {
@@ -63,5 +79,16 @@ fetch('https://api.nasa.gov/insight_weather/?api_key=rSeiuzYdFB4Uhvgqt2HuCaIL4dI
     document.getElementById("day_end").innerHTML = "Day End: " + data[selected].Last_UTC 
     document.getElementById("pre_avg").innerHTML = "Avg: " + data[selected].PRE.av + " Pa" 
     document.getElementById("pre_high").innerHTML = "High: " + data[selected].PRE.mx + " Pa" 
-    document.getElementById("pre_low").innerHTML = "Low: " + data[selected].PRE.mn + " Pa" 
+    document.getElementById("pre_low").innerHTML = "Low: " + data[selected].PRE.mn + " Pa"
+    for (i = 0; i < 16; i++) {
+      if (data[selected].WD[i] != null) {
+        document.getElementById("direction_" + [i+1].toString()).setAttribute(
+            "style", "--ctSize: " + data[selected].WD[i].ct + ";"
+        )
+      } else {
+        document.getElementById("direction_" + [i+1].toString()).setAttribute(
+            "style", "--ctSize: 0;"
+        )
+      }
+    }
   }
